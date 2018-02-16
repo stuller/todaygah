d0ce95743e1aac733ece4da3bddb5c8eb2b16d30
stuller <yiddlemariegold@gmail.com>
2018-02-15 20:22:59 -0600

    strikethrough on done items

--- App.js
+++ App.js
@@ -44,15 +44,18 @@ export default class App extends React.Component {
                         this.state.list.map((item, index) => (
                             <ListItem
                                 key={index}
+                                containerStyle={styles.listItem}
                                 hideChevron
                                 title={
                                     <View>
                                         <CheckBox
                                             containerStyle={styles.listCheckbox}
                                             title={item.title}
-                                            uncheckedIcon='circle'
+                                            uncheckedIcon='circle-thin'
                                             checkedIcon='check-circle'
                                             checked={item.done}
+                                            size={30}
+                                            textStyle={item.done ? styles.doneItemText : styles.itemText}
                                             onPress={() => this.handleCheckItem(index)}
                                         />
                                     </View>
@@ -75,15 +78,33 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
-        borderWidth: 1,
-        borderColor: 'red'
+        justifyContent: 'flex-start',
+        marginTop: 40
     },
     list: {
         width: 200
     },
+    listItem: {
+        paddingTop: 0,
+        paddingRight: 0,
+        paddingBottom: 0,
+        paddingLeft: 0,
+        margin: 0
+    },
     listCheckbox: {
-        'backgroundColor': 'transparent',
-        'borderWidth': 0
+        backgroundColor: 'transparent',
+        borderWidth: 0,
+        marginTop: 0,
+        marginRight: 0,
+        marginBottom: 0,
+        marginLeft:0
+    },
+    itemText: {
+        fontSize: 16
+    },
+    doneItemText: {
+        color: '#aaa',
+        fontSize: 16,
+        textDecorationLine: 'line-through'
     }
 });
